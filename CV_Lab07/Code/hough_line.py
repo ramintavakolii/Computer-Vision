9445a24b871d9bb537851ad8bc0971ed1a47551c
Ramin Tavakoli <Ramin6tavakoli6@gmail.com>
2024-05-02 17:11:57 +0330

    task1 added

--- CV_Lab07/Code/hough_line.py
+++ CV_Lab07/Code/hough_line.py
@@ -1,8 +1,12 @@
 import numpy as np
 import cv2
+import time
+
+
 
 def draw_line(I,rho,theta):
     "draws a line in image 'I' given 'rho' and 'theta'"
+    
     a = np.cos(theta)
     b = np.sin(theta)
     x0 = a*rho
@@ -15,17 +19,31 @@ def draw_line(I,rho,theta):
     cv2.line(I,(x1,y1),(x2,y2),(0,0,255),1)
 
     
-I = cv2.imread('highway.jpg')
-
+I = cv2.imread('./Code/Images/highway.jpg')
 G = cv2.cvtColor(I,cv2.COLOR_BGR2GRAY) # -> grayscale
+print(G.shape)
 
-E = cv2.Canny(G,100,200) # find the edges
+# find the edges
+E = cv2.Canny(G,100,200) 
 
-min_votes = 160 # mininum votes to be considered a line
-distance_resolution = 1 # 1 pixel: resolution of the parameter "rho" (distance to origin)
-angle_resolution = np.pi/180 # pi/180 radians: resolution (bin size) of the parameter "theta" 
+# mininum votes to be considered a line
+min_votes = 750 
+
+# 1 pixel: resolution of the parameter "rho" (distance to origin)
+distance_resolution = 5
+
+# pi/180 radians: resolution (bin size) of the parameter "theta" 
+angle_resolution = 1*np.pi/180 
+
+# Record the current time
+start_time = time.time()
+
 L = cv2.HoughLines(E,distance_resolution,angle_resolution,min_votes)
+# Calculate the elapsed time
+elapsed_time = time.time() - start_time
+print("Time taken to execute cv2.HoughLines():", np.round(elapsed_time,4), "seconds")
 
+print(L[0])
 # draw the lines 
 for [[rho,theta]] in L:
     draw_line(I,rho,theta)
--- CV_Lab07/Code/task1.py
+++ CV_Lab07/Code/task1.py
@@ -1,27 +1,19 @@
 import numpy as np
 import cv2
 
-I = cv2.imread('./Code/Images/samand.jpg')
+I = cv2.imread('./Code/Images/coins.jpg')
+G = cv2.cvtColor(I, cv2.COLOR_BGR2GRAY)
+G = cv2.GaussianBlur(G, (5, 5), 0)
 
-G = cv2.cvtColor(I, cv2.COLOR_BGR2GRAY)  # -> Grayscale
-G = cv2.GaussianBlur(G, (7, 7), 0)     # Gaussian blur
-
-canny_high_threshold = 200
-
+canny_high_threshold = 180
 # minimum no. of votes to be considered as a circle
-min_votes = 100
-
-# minimum distance between the centres of detected circles
+min_votes = 30
 min_centre_distance = 40
-
-# resolution of parameters (centre, radius) relative to image resolution
 resolution = 1
 
 circles = cv2.HoughCircles(G, cv2.HOUGH_GRADIENT, resolution, min_centre_distance,
                            param1=canny_high_threshold,
                            param2=min_votes, minRadius=0, maxRadius=100)
-
-# for opencv 2 use cv2.cv.CV_HOUGH_GRADIENT instead of cv2.HOUGH_GRADIENT
 
 # find the edges
 E = cv2.Canny(G, canny_high_threshold/2, canny_high_threshold)
@@ -37,8 +29,12 @@ for c in circles[0, :]:
     # draw the circle center
     cv2.circle(I, (x, y), 2, (0, 0, 255), 2)
 
+print(circles.shape)
+
+n = circles.shape[1]
+font = cv2.FONT_HERSHEY_SIMPLEX
+cv2.putText(I, 'There are %d coins!' % n, (400, 40), font, 1, (255, 0, 0), 2)
 
 cv2.imshow("E", E)
 cv2.imshow("I", I)
 cv2.waitKey(0)
-cv2.destroyAllWindows()

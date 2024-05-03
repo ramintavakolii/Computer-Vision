d3ea31bb2fca97202207c404c1ff62fe1b70bfb3
Ramin Tavakoli <Ramin6tavakoli6@gmail.com>
2024-05-03 11:30:51 +0330

    similarity.py updated

--- CV_Lab08/Code/similarity.py
+++ CV_Lab08/Code/similarity.py
@@ -1,24 +1,24 @@
 import cv2
 import numpy as np
 
-I = cv2.imread('karimi.jpg')
+I = cv2.imread('./Code/Images/karimi.jpg')
 
 tx = 100
 ty = 60
 
-th =  20 # angle of rotation (degrees)
-th *= np.pi / 180 # convert to radians
+th = 20  # angle of rotation (degrees)
+th *= np.pi / 180  # convert to radians
 
-s = 0.6 # scale factor
+s = 2  # scale factor
 
-M = np.array([[s*np.cos(th),-s*np.sin(th),tx],
-              [s*np.sin(th), s*np.cos(th),ty]])
+M = np.array([[s*np.cos(th), -s*np.sin(th), tx],
+              [s*np.sin(th), s*np.cos(th), ty]])
 
-output_size = (I.shape[1], I.shape[0])
-J = cv2.warpAffine(I,M,  output_size)
+output_size = (int(I.shape[1] * s * 2), int(I.shape[0] * s * 2))
+J = cv2.warpAffine(I, M,  output_size)
 
-cv2.imshow('I',I)
+cv2.imshow('I', I)
 cv2.waitKey(0)
 
-cv2.imshow('J',J)
+cv2.imshow('J', J)
 cv2.waitKey(0)
